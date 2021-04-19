ebf30edfcff0ddcb61fd898c4f521cce0b91147b
ekowa123 <78913294+ekowa123@users.noreply.github.com>
2021-04-19 16:21:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask inside Docker ekoooooooo!!"
+    return "Flask inside Docker ekoooooooo0000!"
 
 
 if __name__ == "__main__":
